e7aeb71b7f050752890ddcd3b3e59668ab231b2a
DESKTOP-SN9KF5O\Strawberry <berichandev@gmail.com>
2025-10-26 02:15:50 +0000

    Handle all possible mad haxx shiny rolls (including alphas)

--- ZAShinyWarper/ShinyRollChecker.cs
+++ ZAShinyWarper/ShinyRollChecker.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,10 @@ namespace PLAWarper
 {
     public static class ShinyRollChecker<T> where T : PKM, new()
     {
-        public static int CheckValidDirtyZARNG(T pkm) // Dirty placeholder until we have a proper encounter/param input, partially leveraged from PKHeX
+        public static int CheckValidDirtyZARNG(T pkm, int fivCheck = -1) // Dirty placeholder until we have a proper encounter/param input, partially leveraged from PKHeX
         {
             var p = new GenerateParam9();
-            var fiv = pkm.FlawlessIVCount;
+            var fiv = fivCheck == -1 ? pkm.FlawlessIVCount : fivCheck;
 
             var ec = pkm.EncryptionConstant;
             var seeds = new XoroMachineConsecutive(ec, pkm.ID32);
@@ -29,27 +30,32 @@ namespace PLAWarper
                 ul = xoro.NextInt();
                 while (ul != pkm.PID && tries++ < 90001) // check 90001 rolls for hax
                     ul = xoro.NextInt();
+                if (ul != pkm.PID)
+                    continue;
                 const int UNSET = -1;
                 const int MAX = 31;
                 Span<int> ivs = [UNSET, UNSET, UNSET, UNSET, UNSET, UNSET];
-                if (p.IVs.IsSpecified)
-                {
-                    p.IVs.CopyToSpeedLast(ivs);
+                Span<int> ivspk = [pkm.IV_HP, pkm.IV_ATK, pkm.IV_DEF, pkm.IV_SPA, pkm.IV_SPD, pkm.IV_SPE];
+
+                // We don't have encounter data for now, so we just skip possible flawless IVs
+                if (fiv > 0)
+                { 
+                    for (int j = 0; j < 6; j++)
+                    {
+                        bool isFlawless = ivspk[j] == MAX;
+                        if (ivs[j] == UNSET && !isFlawless)
+                            ivs[j] = (int)xoro.NextInt(MAX + 1);
+                        else
+                            ivs[j] = MAX;
+                    }
                 }
                 else
                 {
-                    for (int j = 0; j < fiv; j++)
+                    for (int j = 0; j < 6; j++)
                     {
-                        int index;
-                        do { index = (int)xoro.NextInt(6); }
-                        while (ivs[index] != UNSET);
-                        ivs[index] = MAX;
+                        if (ivs[j] == UNSET)
+                            ivs[j] = (int)xoro.NextInt(MAX + 1);
                     }
-                }
-                for (int j = 0; j < 6; j++)
-                {
-                    if (ivs[j] == UNSET)
-                        ivs[j] = (int)xoro.NextInt(32);
                 }
                 if (pkm.IV_HP != ivs[0])
                     continue;
@@ -66,6 +72,8 @@ namespace PLAWarper
                 Console.WriteLine($"Valid seed: {s} / EC: {ec} / {pkm.FileName} Rolls: {tries}");
                 return tries;
             }
+            if (fiv > 0) // no encounter data, we don't know if we naturally rolled flawless IVs
+                return CheckValidDirtyZARNG(pkm, fiv - 1);
             return -1;
         }
     }
